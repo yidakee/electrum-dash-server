d10a55bb50c835cff8549f7e08598294a1611301
Propulsion <Propulsion@DarkcoinTalk.org>
2015-02-03 15:00:43 -0500

    updated GenesisHash in storage.py

--- src/storage.py
+++ src/storage.py
@@ -8,7 +8,8 @@ from processor import print_log, logger
 from utils import bc_address_to_hash_160, hash_160_to_pubkey_address, hex_to_int, int_to_hex, Hash
 
 global GENESIS_HASH
-GENESIS_HASH = '000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f'
+GENESIS_HASH = '00000ffd590b1485b3caadc19b22e6379c733355108f107a430458cdf3407ab6' #Darkcoin src/chainparams.cpp L62 
+    },
 
 """
 Patricia tree for hashing unspents
@@ -105,7 +106,7 @@ class Storage(object):
     def listunspent(self, addr):
         key = self.address_to_key(addr)
         if key is None:
-            raise BaseException('Invalid Bitcoin address', addr)
+            raise BaseException('Invalid Darkcoin address', addr)
 
         out = []
         for k, v in self.db_utxo.iterator(start=key):
