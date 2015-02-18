5978cc258fd2050d09fb495cbde7720b0107ab7c
Propulsion <Propulsion@DarkcoinTalk.org>
2015-02-17 20:17:35 -0500

    Darkcoin: Updated src/storage.py for testnet

--- src/storage.py
+++ src/storage.py
@@ -8,7 +8,7 @@ from processor import print_log, logger
 from utils import bc_address_to_hash_160, hash_160_to_pubkey_address, hex_to_int, int_to_hex, Hash
 
 global GENESIS_HASH
-GENESIS_HASH = '00000ffd590b1485b3caadc19b22e6379c733355108f107a430458cdf3407ab6' #Darkcoin src/chainparams.cpp L62 
+GENESIS_HASH = '00000bafbc94add76cb75e2ec92894837288a481e5c005f6563d91623bf8bc2c' #Testnet Darkcoin src/chainparams.cpp L129
     
 
 """
